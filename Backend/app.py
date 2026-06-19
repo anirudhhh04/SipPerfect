4759f5aa2621ce5ec308f7012350355bac4ebb4d
ANIRUDH P <anirudh.p@iouring.com>
2026-06-19 10:58:48 +0530

    Updated backend

--- Backend/app.py
+++ Backend/app.py
@@ -4,6 +4,7 @@ from flask import jsonify
 from datetime import datetime, timedelta
 from flask_cors import CORS
 import joblib
+import pandas as pd
 app = Flask(__name__)
 CORS(app)
 model = joblib.load("model.pkl")
@@ -17,16 +18,24 @@ def home():
 @app.route("/predict", methods=["POST"])
 def predict():
     data = request.get_json()
-    
+
     tea_temp = data["tea_temp"]
     room_temp = data["room_temp"]
     fan_speed = data["fan_speed"]
     volume_ml = data["volume_ml"]
     cup_material = data["cup_material"]
 
-    prediction = model.predict([[tea_temp,room_temp,fan_speed,volume_ml,cup_material]])[0]
+    input_data = pd.DataFrame(
+        [[tea_temp, room_temp, fan_speed, volume_ml, cup_material]],
+        columns=["tea_temp","room_temp","fan_speed","volume_ml","cup_material"]
+    )
+    prediction = model.predict(input_data)[0]
     minutes = int(round(float(prediction), 2))
-    ready_time = ( datetime.now() +timedelta(minutes=minutes)).strftime("%I:%M %p") #for real time prediction
-    return jsonify({"drinkable_time": minutes,"ready_at": ready_time,"message": f"Your tea will be perfect in {minutes} minutes"})
+    ready_time = (datetime.now() + timedelta(minutes=minutes)).strftime("%I:%M %p")
+    return jsonify({
+        "drinkable_time": minutes,
+        "ready_at": ready_time,
+        "message": f"Your tea will be perfect in {minutes} minutes"
+    })
 if __name__ == "__main__":
     app.run(debug=True)
